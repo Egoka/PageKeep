7ee82d3670b8c465c2e69eae216340a6b3d86400
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-27 16:05:57 +0300

    Router applications
    + Created todo.js file, where event handlers for the index.html page are declared.
    ++ At the beginning of the file, the 'express' library is imported from which the Router object is taken. By which, in turn, an instance of router is created, of the Router() class.
    ++ 4 methods are created using the router object:
    +++ router.get() to display the page, namely getting the task list
    +++ router.post() for getting external data, namely creating new posts
    +++ router.put() to edit entries from the list
    +++ router.delete() to remove entries from the list
    ++ As a result of the work, the resulting router is exported.
    + In the index.js file, the created router is imported under the name todoRoutes and used in the app.use() method at '/api/todo'.
    ===============
    ===============
    ===============
    + Создан файл todo.js, где объявлены обработчики событий для страницы index.html.
    ++ В начале файла импортируется библиотека 'express', из которой достается объект Router. По которому в свою очередь создается экземпляр router, класса Router().
    ++ С помощью объекта router создано 4 метода:
    +++ router.get() для отображения страницы, а именно получение списка задач
    +++ router.post() для получения внешних данных, а именно создания новых записей
    +++ router.put() для редактирования записей из списка
    +++ router.delete() для удаления записей из списка
    ++ По результату работы полученный router экспортируется.
    + В файле index.js созданный router импортируется  под именем todoRoutes и используется в методе app.use() по адресу '/api/todo'.

---
+++ routes/todo.js
@@ -0,0 +1,11 @@
+const{Router}= require('express')
+const router = Router()
+//Получение списка задач
+router.get('/',(req,res)=>{})
+//Создание новых записей
+router.post('/',(req,res)=>{})
+//Изменение задачи
+router.put('/:id',(req,res)=>{})
+//Удаление адачи
+router.delete('/:id',(req,res)=>{})
+module.exports = router
