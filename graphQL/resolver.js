8bb9e534ac567417fe66ad3d4f3625b25215f000
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-29 20:37:54 +0300

    Learning GraphQL
    + In the schema.js file, a "Query" type with an internal "test" field with a required data type "TestType!"
    ++ Next to it, the data type "type TestType" is declared, which has two data fields "count", which has integer values, and "users", which is an array.
    ++ Next, the data type "type User" is created, which consists of three data fields: "name" of string type, "email" of string type, and "age" of integer type.
    + In resolver.js, the test() method is exported, which returns an object from "count" that is generated and "users" that is described above.
    + To display data, you need to turn to the "query" type, then to "test" where you can request either the "count" field, or "users" for which you need to declare the fields you want to display.
    ===============
    ===============
    ===============
    + В файле schema.js создан тип «Query» с внутренним полем «test» с обязательным типом данных «TestType!»
    ++ Рядом объявлен тип данных «type TestType» у которого есть два поля данных «count», который имеет целочисленные значения, и «users» который является массивом.
    ++ Далее создан тип данных «type User», который состоит из трех полей данных: «name» строчного типа, «email» строчного типа и «age» целочисленного типа.
    + В файле resolver.js экспортируется метод test(), который возвращает объект из «count», который генерируется и «users»  который описан выше.
    + Для вывода данных необходимо обратиться к типу «query», потом к «test» где можно запросить либо поле «count», либо «users» для которого необходимо объявить поля которые необходимо вывести.
    ===============
    ===============
    ===============
    INPUT
    query{
      test{
        users{
          name age email
        }
      }
    }
    ===============
    OUTPUT
    {
      "data": {
        "test": {
          "users": [
            {
              "name": "Egor",
              "age": 30,
              "email": "Egor@gmail.com"
            },
            {
              "name": "Ivan",
              "age": 36,
              "email": "Ivan@gmail.com"
            }
          ]
        }
      }
    }

--- graphQL/resolver.js
+++ graphQL/resolver.js
@@ -1,2 +1,12 @@
+const users =[
+    {name:'Egor', age: 30, email: 'Egor@gmail.com'},
+    {name:'Ivan', age: 36, email: 'Ivan@gmail.com'}
+]
 module.exports = {
+    test(){
+        return {
+            count:Math.trunc(Math.random()*10),
+            users
+        }
+    }
 }
--- graphQL/schema.js
+++ graphQL/schema.js
@@ -1,3 +1,15 @@
 const {buildSchema} = require('graphql')
 module.exports = buildSchema(`
+    type User{
+        name: String!
+        email: String!
+        age: Int!
+    }
+    type TestType{
+        count: Int!
+        users: [User!]!
+    }
+    type Query{
+        test:TestType!
+    }
 `)
