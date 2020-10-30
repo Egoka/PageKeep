7c8a67b55d0c78ebd1d9403c71d24eafd62119e8
Egor Bondarenko <eg.bonda2014@gmail.com>
2020-10-30 22:49:50 +0300

    Deploy to Heroku

---
+++ keys/dataBase-PROD.js
@@ -0,0 +1,9 @@
+module.exports = {
+    DB_NAME:process.env.DB_NAME,
+    USER_NAME:process.env.USER_NAME,
+    PASSWORD:process.env.PASSWORD,
+    CONFIGURATION:{
+        host:process.env.HOST,
+        port: '3306'
+    }
+}
